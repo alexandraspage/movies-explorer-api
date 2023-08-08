9127c28e60120a5301bb3bddf9e6b62ccf71516c
Sasha Dudina <aleksad301@gmail.com>
2023-08-08 14:58:50 +0300

    порт

--- utils/config.js
+++ utils/config.js
@@ -7,7 +7,7 @@ const LIMITER = rateLimit({
   legacyHeaders: false,
 });
 
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 4000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const NO_ERROR = 200;
 const CREATED = 201;
